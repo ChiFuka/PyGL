b5fbe690658c0e43f08363cb4aba1d9ad9aa24a6
ChiFuka <kovanyov@gmail.com>
2016-08-22 15:41:15 +0300

    deviations

--- deviations.py
+++ deviations.py
@@ -1,4 +1,21 @@
 def dict_dist(a={}, b={}):
-    print a.keys() - b.keys()
 
-print dict_dist ({'a':'horse','b':'rope'}, {'a':'horse','b':'herb', 'c':'exception'})
+    cnt = 0
+    if len(a.keys())== len(b.keys()):
+        for k in a:
+            if a[k] != b[k]:
+                cnt+=1
+    elif len(a.keys()) < len(b.keys()):
+        for k in a:
+            if a[k] != b[k]:
+                cnt+=1
+        cnt = cnt + (len(b.keys()) - len(a.keys()))
+    elif len(a.keys()) > len(b.keys()):
+        for k in b:
+            if b[k] != a[k]:
+                cnt+=1
+        cnt = cnt + (len(a.keys()) - len(b.keys()))
+    return cnt
+
+
+print dict_dist ({'a':'horse','b':'rope', 'c':'exception'}, {'a':'hrrse'})
